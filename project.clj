ec6732762afb333db1db1ff94282d5533f47dc87
Tim McIver <tim@timmciver.com>
2014-10-13 15:12:45 -0400

    Added a URL to the project file.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject obr-clj "0.1.0-SNAPSHOT"
   :description "A Clojure wrapper for the Felix bundle repository library."
-  :url ""
+  :url "https://github.com/tmciver/obr-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
