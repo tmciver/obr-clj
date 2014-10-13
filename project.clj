5dedc11e56ba024e87739b7d16bdeafdcf7ab23d
Tim McIver <tim@timmciver.com>
2014-10-13 15:17:17 -0400

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject obr-clj "0.1.0"
+(defproject obr-clj "0.1.1-SNAPSHOT"
   :description "A Clojure wrapper for the Felix bundle repository library."
   :url "https://github.com/tmciver/obr-clj"
   :license {:name "Eclipse Public License"
